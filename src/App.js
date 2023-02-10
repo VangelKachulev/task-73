4ee80ab2a0167450d718462b5d48661b1246c06c
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-11 00:15:53 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
-import { Counter } from "./Counter";
+import { useEffect, useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-       <Counter></Counter>
-    </div>
-  );
+
+  let [counter , setCounter ] = useState(1);
+
+    let myFunction = () => {
+        setCounter(counter + 1)
+
+    }
+    useEffect(() => {
+        document.title = `Count (${counter})`
+    }, [counter])
+   
+
+    return (
+        <button onClick={myFunction}>Count ({counter})</button>
+    )
+ 
 }
 
 export default App;
